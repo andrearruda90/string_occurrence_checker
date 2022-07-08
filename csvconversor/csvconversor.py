bc5c65d3b94738b69ff85f9838a2b999f8f4a221
André Arruda <andre.todimo@gmail.com>
2022-07-08 14:48:10 -0400

    ..

--- csvconversor/csvconversor.py
+++ csvconversor/csvconversor.py
@@ -1,10 +1,8 @@
 import numpy as np 
 from pandas import read_excel 
-import pandas as pd
-import os 
-dir_path = os.path.dirname(str(os.path.realpath("csvconversor.py") + "scdkst00.xlsx"))
+import pandas as pd 
 df_new = pd.read_csv("C:/Users/andre.arruda/Desktop/scdkst00.csv", sep=None) 
-GFG = pd.ExcelWriter(dir_path) 
+GFG = pd.ExcelWriter("C:/Temp/scdkst00.xlsx") 
 df_new.to_excel(GFG, index = False) 
- 
+  
 GFG.save()
